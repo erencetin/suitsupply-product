3a079219e6b1f1c21c2faf60914e83a0ea0efe70
eren <eecetin@gmail.com>
2018-08-07 02:19:03 +0200

    added some files.

--- SuitSupplyAssessment.ProductCatalog.Model/Product.cs
+++ SuitSupplyAssessment.ProductCatalog.Model/Product.cs
@@ -10,7 +10,6 @@ namespace SuitSupplyAssessment.ProductCatalog.Model
         public string Code { get; set; }
         public string Name { get; set; }
         public string Photo { get; set; }
-       // [Range(0, int.MaxValue, ErrorMessage = "Please enter a price bigger than {1}")]
         public decimal Price { get; set; }
         public DateTime LastUpdated { get; set; }
     }
